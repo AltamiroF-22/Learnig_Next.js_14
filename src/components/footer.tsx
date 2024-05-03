6633358288294400281c1413d997dc593314443b
Altamiro Júnior <124850908+AltamiroF-22@users.noreply.github.com>
2024-05-03 17:54:22 -0300

    layouts Routes

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="w-screen bg-blue-950 text-center p-3 absolute bottom-0">
+      <p className="text-slate-50">footer</p>
+    </footer>
+  );
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header className="w-screen bg-blue-950 text-center p-3">
+      <p className="text-slate-50">header</p>
+    </header>
+  );
+}
