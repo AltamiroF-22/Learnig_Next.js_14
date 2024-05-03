b42a558ca405f2e98e237aac9ad10353d814fd86
Altamiro Júnior <124850908+AltamiroF-22@users.noreply.github.com>
2024-05-03 14:07:53 -0300

    private route

---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,3 @@
+export default function privateRout() {
+  return <h1>You can&apos;t view this in the browser</h1>;
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function BarChart() {
+  return <h1>BarChart</h1>;
+}
