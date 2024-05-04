5b7bd6c8d0767c2517ddcb49076fe057a14643da
Altamiro Júnior <124850908+AltamiroF-22@users.noreply.github.com>
2024-05-04 14:51:34 -0300

    Routing Metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,10 +1,24 @@
-export default function ProductDetails({
-  params,
-}: {
+import { Metadata } from "next";
+interface Props {
   params: {
     productId: string;
   };
-}) {
+}
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iPhone ${params.productId}`);
+    }, 1000);
+  });
+  return {
+    title: `Product ${title}`,
+  };
+};
+
+export default function ProductDetails({ params }: Props) {
   return (
     <>
       <h1>Product Details {params.productId}</h1>
