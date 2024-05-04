1891f78a4575c1c37c1d1012f758770ae885b3a1
Altamiro Júnior <124850908+AltamiroF-22@users.noreply.github.com>
2024-05-04 15:48:48 -0300

    Link Component Navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <main>
+      <h1>Home Page</h1>
+      <Link className="underline p-1 text-blue-700" href="/blog">
+        Blog
+      </Link>
+      <Link className="underline p-1 text-blue-700" href="/products">
+        Products
+      </Link>
+    </main>
+  );
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,50 @@
+import Link from "next/link";
+
+interface LinkProps {
+  _id: string;
+  text: string;
+}
+
 export default function Products() {
+  const data: LinkProps[] = [
+    {
+      _id: "1",
+      text: "Product 1",
+    },
+    {
+      _id: "2",
+      text: "Product 2",
+    },
+    {
+      _id: "3",
+      text: "Product 3",
+    },
+    {
+      _id: "4",
+      text: "Product 4",
+    },
+    {
+      _id: "5",
+      text: "Product 5",
+    },
+  ];
   return (
     <>
-      <h1>Products List</h1>
-      <h2>Product 1</h2>
-      <h2>Product 2</h2>
-      <h2>Product 3</h2>
+      <Link href="/" className="underline p-1 text-blue-700">
+        Home
+      </Link>
+      <h1 className="text-3xl text-rose-600">Products List</h1>
+
+      {data.map((item) => (
+        <h2 key={item._id}>
+          <Link href={`products/${item._id}`}>{item.text}</Link>
+        </h2>
+      ))}
+      <h2>
+        <Link href={`products/6`} replace>
+          Product 6 (back to home)
+        </Link>
+      </h2>
     </>
   );
 }
